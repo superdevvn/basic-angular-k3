d593ed601fe69b126be418e4e152f8ed8b82ec0e
William Cao <williamcao.cmu@gmail.com>
2018-03-04 14:47:06 +0700

    hi!

--- students/Lê Văn Hậu/ES6/es6-template-literals.js
+++
@@ -1,9 +0,0 @@
-function fullName(firstName, lastName) {
-    // OLD
-    // var fullName = firstName + " " + lastName;
-    // alert(fullName);
-    // ES6
-    var fullName = `My name is: ${firstName} ${lastName}`;
-    alert(fullName);
-}
-fullName("Dark","Peter");
