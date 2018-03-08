71735af66ddce28a0b8e1921397839cd20a73828
Duy Nguyen <nguyenphuongduy124@gmail.com>
2018-03-08 14:50:09 +0700

    -BTVN-01

---
+++ students/phuongduy/ES6/BTVN-01/index.ts
@@ -0,0 +1,4 @@
+import * as t1 from './ts-default-parameters'
+import * as t2 from './multiline-string'
+t1.lienket(undefined, undefined, undefined);
+t2.say("Duy", "Nguyen", 22);
---
+++ students/phuongduy/ES6/BTVN-01/multiline-string.ts
@@ -0,0 +1,5 @@
+export function say(firstName: string, lastName: string, age: number) {
+    var message = `Hi, I am ${firstName} ${lastName},
+                    I am ${age} years old`;
+    alert(message);
+}
