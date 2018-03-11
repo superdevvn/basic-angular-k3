004f5f8771a277d6672079888776810250a71b2a
DarkPeter <darkpeter@DarkPeters-MacBook-Air.local>
2018-03-11 15:36:40 +0700

    sample-angular

---
+++ source demo/sample-angular/src/app/test/test.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+  firstName: string;
+  lastName:string;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  hello(){
+    let fullName = `${this.firstName} ${this.lastName}`;
+    alert(fullName);
+  }
+}
