dd68bea684a3b8d955de5b69b2a22fed96c17d2f
DESKTOP-AH2IB63\Windows 10 <leehuunghi@gmail.com>
2018-03-15 21:31:25 +0700

    commit lan 3 tren lop

---
+++ students/lehuunghi/sample-Router/src/app/layout-no1/layout-no1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-no1',
+  templateUrl: './layout-no1.component.html',
+  styleUrls: ['./layout-no1.component.css']
+})
+export class LayoutNo1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
