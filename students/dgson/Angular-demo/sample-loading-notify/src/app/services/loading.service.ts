c5bbdc31530e307284b906834ae9d5c337503fa0
giangson1604 <dgs160495@gmail.com>
2018-03-25 17:17:23 +0700

    -update loading notify

---
+++ students/dgson/Angular-demo/sample-loading-notify/src/app/services/loading.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import * as $ from 'jquery';
+
+@Injectable()
+export class LoadingService {
+
+  constructor() { }
+
+  star() {
+    $('body').append(`<any id = "globalloading"
+    style = "top:0;
+    left: 0;
+    height:100vh;
+    width:100vw;
+    position:fixed;
+    opacity:0.9;
+    background: #FFFFFF;
+    z-index: 99999;
+    background-image: url(../assets/loading.gif);
+    background-position:center;
+    background-repeat:no-repeat;"
+    ></any>`)
+  }
+
+  stop() {
+    $('#globalloading').remove();
+  }
+}
