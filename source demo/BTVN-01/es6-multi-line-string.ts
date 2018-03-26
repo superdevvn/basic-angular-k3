62bb1d352c5e14cef6c192c5f61df25e63fe502a
Trần Thuận Nghĩa <ghost.of.dark.peter@gmail.com>
2018-03-27 00:45:05 +0700

    Remove old folder&file

--- source demo/BTVN-01/es6-multi-line-string.ts
+++
@@ -1,5 +0,0 @@
-export function say(firstName:string, lastName:string, age:number){
-    var message = `Hi, I am ${firstName} ${lastName}, 
-    I am ${age} years old`;
-    alert(message);
-}
--- source demo/BTVN-01/index.ts
+++
@@ -1,5 +0,0 @@
-import * as t1 from './es6-default-parameter';
-import * as t2 from './es6-multi-line-string';
-
-t1.link(undefined,undefined,undefined);
-t2.say("Dark","Peter",27);
