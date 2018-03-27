c779b1d8d7adbba9681208e31c74ca0f71a26eca
giangson1604 <dgs160495@gmail.com>
2018-03-28 02:24:59 +0700

    update btvn-4

---
+++ students/dgson/BTVN-4/AddTheme/src/app/user/user-detail.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { UserService } from './user.service';
+import { NotifyService } from '../../services/notify.service';
+
+@Component({
+  selector: 'app-userdetail',
+  templateUrl: './user-detail.component.html',
+})
+export class UserDetailComponent implements OnInit {
+  id: number;
+  user:  any = {};
+  
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService,
+    private notifyService: NotifyService
+
+  ) { }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(params=>{
+      this.id = +params['id'];
+      if (this.id > 0) {
+        this.userService.getUser(this.id).then(user=>{
+          this.user = user;
+        })
+      }
+    });
+  }
+
+  save() {
+    this.userService.saveUser(this.user).then(user=>{
+      this.notifyService.success("OK con de");
+    }).catch(err=>{
+      this.notifyService.error("sai roi ban ei!!!");
+    })
+  }
+
+}
